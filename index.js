fc41265667cad1901058c236328d8fe28faec4c4
Aditya <aditya.baravkar@gmail.com>
2021-06-20 12:13:26 +0530

    Change background color

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+var image_url;
+const api_call = () => {
+    const URL = 'https://api.unsplash.com/photos/random?query=drum'
+    fetch(URL, {
+        headers : {
+            'Authorization' : 'Client-ID 3KwYJC8ztFLH-Ypp3Q0bKexakRc85lAwFlfQi4mVUd0'
+        }
+    }).then(res => res.json()).then(res => {image_url=res.urls.small; change_background(image_url)}).catch(err=>console.log(err))
+}
+api_call()
+
+
+const change_background = (image_src) => {
+    let container_style = document.getElementsByClassName("container")[0].style
+    let bg_color = getComputedStyle(document.documentElement).getPropertyValue("--background_low")
+    container_style.background = `linear gradient(300deg, ${bg_color}, ${bg_color}), url(${image_src})`
+
+}
+
 
 const animate = (key) => {
     const currKey = document.querySelector(`.${key}`)
@@ -84,3 +103,45 @@ var btnAr = document.querySelectorAll(".pads")
 for(let i=0; i<btnAr.length; i++) {
     btnAr[i].addEventListener("click", handleClickDrum)
 }
+
+
+//theme-1
+const theme_1__background = "#091921"
+const theme_1__background_low = "rgba(9,25,33,0.8)"
+const theme_1__text = "#00fff1"
+
+//theme-2
+const theme_2__background = "#f7c340"
+const theme_2__background_low = "rgba(247,195,64,0.85)"
+const theme_2__text = "#2d2d2d"
+
+
+const changeTheme = (theme) => {
+    let root = document.documentElement
+    if(theme === "theme_1") {
+        root.style.setProperty("--background", theme_1__background)
+        root.style.setProperty("--background_low", theme_1__background_low)
+        root.style.setProperty("--text", theme_1__text)
+    } else {
+        root.style.setProperty("--background", theme_2__background)
+        root.style.setProperty("--background_low", theme_2__background_low)
+        root.style.setProperty("--text", theme_2__text)
+    }
+}
+
+
+var curr_theme = "theme_1"
+const themeChanger = document.getElementById("util__button-theme")
+themeChanger.addEventListener("click", (e)=> {
+    themeChanger.classList.add("change_theme__pressed")
+    setInterval(()=>{
+        themeChanger.classList.remove("change_theme__pressed")
+    }, 200)
+    if(curr_theme==="theme_1") {
+        changeTheme("theme_2")
+        curr_theme = "theme_2"
+    } else {
+        changeTheme("theme_1")
+        curr_theme = "theme_1"
+    }
+})
